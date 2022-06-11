c9ede236f8357dc40480b07ca8e129d181f8595f
Kiril Kaloyanov <83544294+KirilKaloyanov@users.noreply.github.com>
2022-06-11 16:23:24 +0300

    JS basic syntax and conditional statements

---
+++ JS/01-Fundamentals/01-Basics/Lab/04.Foreign_languages.js
@@ -0,0 +1,13 @@
+function dict(country) {
+    switch (country) {
+        case 'USA':
+        case 'England': console.log('English');
+            break;
+        case 'Spain':
+        case 'Argentina':
+        case 'Mexico': console.log('Spanish')
+            break;
+        default: console.log('unknown');
+    }
+}
+dict('Mexico');
